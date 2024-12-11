e17e6184472ce25118f72f4db91be34c78fa4f0a
Dan Steadman <dan.steadman.12@gmail.com>
2024-12-11 01:25:01 -0700

    Making ready for github pages publish

--- apps/web/next.config.ts
+++ apps/web/next.config.ts
@@ -1,11 +1,14 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
-  env: {
-    MAPBOX_ACCESS: process.env.MAPBOX_ACCESS,
-    SERVER_URL: process.env.SERVER_URL,
-  }
+	/* config options here */
+	env: {
+		MAPBOX_ACCESS: process.env.MAPBOX_ACCESS,
+		SERVER_URL: process.env.SERVER_URL,
+	},
+	output: "export",
+	reactStrictMode: true,
+	// basePath: "/contactPlanner"
 };
 
 export default nextConfig;
